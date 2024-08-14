db27487f8402d0e3ebb94ec235548bab16107df0
vitorialuz229 <vitorialuz831@gmail.com>
2024-08-14 12:00:43 -0300

    feature/Lista de exercicíos laços de repetição

---
+++ Lista de Exercicios - Laços de Repetição/Question04.java
@@ -0,0 +1,14 @@
+package Exercicios.aula17.ListaExercicios;
+
+import java.util.Scanner;
+
+public class Question04 {
+          public static void main(String[] args) {
+               Scanner scan = new Scanner(System.in);
+               
+               System.out.println("População do país A:");
+               int populaçãoA = scan.nextInt();
+
+               
+          }
+}
